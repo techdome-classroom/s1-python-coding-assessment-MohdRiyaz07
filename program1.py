b1c3379b593cb1c90232815a7fe277e71d199840
unknown <digitalriiyaz@gmail.com>
2024-11-11 14:31:10 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,5 +2,21 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-                    
-        return 0
+        if not grid:
+            return 0
+            
+        def dfs(r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 'W':
+                return 
+            grid[r][c] = 'W'
+            dfs(r - 1, c)
+            dfs(r + 1, c)
+            dfs(r, c - 1)
+            dfs(r, c + 1)
+        ans = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 'L':
+                    dfs(r, c)
+                    ans += 1
+        return ans
